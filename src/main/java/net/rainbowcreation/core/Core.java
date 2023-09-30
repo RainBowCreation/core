9c0e941cf1d64ace2589e0452625a2cae1102a03
VocanicZ <ampnattahapat@gmail.com>
2023-09-30 12:50:22 +0700

    clone from NoMulti change gradle to maven

--- src/main/java/net/rainbowcreation/core/Core.java
+++ src/main/java/net/rainbowcreation/core/Core.java
@@ -1,17 +1,52 @@
 package net.rainbowcreation.core;
 
+import com.earth2me.essentials.Essentials;
+import net.rainbowcreation.core.chat.Console;
+import net.rainbowcreation.core.datamanager.Config;
+import net.rainbowcreation.core.datamanager.Service;
+import net.rainbowcreation.core.eventmanager.*;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Core extends JavaPlugin {
+    private static Core instance;
+    public static Core getInstance(){
+        return instance;
+    }
+    public Config guiData;
+    public Config playerData;
+    public Essentials ess;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        setupConfig();
+        Manager.register(Bukkit.getPluginManager(), instance);
+        if (instance.getConfig().contains("Header")) {
+            for (Object txt : instance.getConfig().getList("Header")) {
+                Console.info(txt.toString());
+            }
+        }
+        ess = (Essentials) instance.getServer().getPluginManager().getPlugin("Essentials");
+        if (ess == null) {
+            Console.info("Please install Essentials"); //checker
+        }
+        Service.initialize();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.getServer().getScheduler().cancelTasks(instance);
+    }
+
+    private void setupConfig() {
+        saveDefaultConfig();
+        guiData = new Config(instance, "gui.yml");
+        guiData.saveConfig();
+        guiData.reloadConfig();
+        playerData = new Config(instance, "userdata.yml");
+        playerData.saveConfig();
+        playerData.reloadConfig();
     }
 }
