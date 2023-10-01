2be3ab5f5cc0acd73b63da9c389b87e185d62447
VocanicZ <ampnattahapat@gmail.com>
2023-10-02 03:09:51 +0700

    update unlined header fix config.yml

--- src/main/java/net/rainbowcreation/core/Core.java
+++ src/main/java/net/rainbowcreation/core/Core.java
@@ -8,6 +8,9 @@ import net.rainbowcreation.core.eventmanager.*;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Arrays;
+import java.util.List;
+
 public final class Core extends JavaPlugin {
     private static Core instance;
     public static Core getInstance(){
@@ -20,6 +23,15 @@ public final class Core extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+        List<String> header = Arrays.asList("#####################################################################################",
+                                            "#  _____       _       ____                 _____                _   _              #",
+                                            "# |  __ \\     (_)     |  _ \\               / ____|              | | (_)             #",
+                                            "# | |__) |__ _ _ _ __ | |_) | _____      _| |     _ __ ___  __ _| |_ _  ___  _  __   #",
+                                            "# |  _  // _` | | '_ \\|  _ < / _ \\ \\ /\\ / / |    | '__/ _ \\/ _`  | __| |/ _ \\| '_ \\  #",
+                                            "# | | \\ \\ (_| | | | | | |_) | (_) \\ V  V /| |____|  | |  __/ (_| | |_| | (_) | | | | #",
+                                            "# |_|  \\_\\__,_|_|_| |_|____/ \\___/ \\_/\\_/  \\_____|_|  \\___|\\__,_|\\__|_|\\___/|_|  |_| #",
+                                            "#                                                                                   #",
+                                            "###########################################################################core######");
         setupConfig();
         Manager.register(Bukkit.getPluginManager(), instance);
         if (instance.getConfig().contains("Header")) {
