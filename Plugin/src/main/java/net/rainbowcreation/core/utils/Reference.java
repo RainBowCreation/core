4be9cbd7b24a7e149e31052737848a8a7dd74fec
NathanielSong <contact@rainbowcreation.net>
2023-12-26 16:26:46 +0700

    update 7.0.1

--- Plugin/src/main/java/net/rainbowcreation/core/utils/Reference.java
+++ Plugin/src/main/java/net/rainbowcreation/core/utils/Reference.java
@@ -2,5 +2,5 @@ package net.rainbowcreation.core.utils;
 
 public final class Reference {
     public static String NAME = "Core";
-    public static String VERSION = "0.8.0-DEV";
+    public static String VERSION = "7.0.1-DEV";
 }
