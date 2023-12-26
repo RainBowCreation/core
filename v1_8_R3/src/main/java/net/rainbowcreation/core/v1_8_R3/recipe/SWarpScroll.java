977f2afeb10f0b0c0b82a22abcdb944e6e282c16
NathanielSong <contact@rainbowcreation.net>
2023-12-26 19:46:41 +0700

    edit warp_scroll description

--- v1_8_R3/src/main/java/net/rainbowcreation/core/v1_8_R3/recipe/SWarpScroll.java
+++ v1_8_R3/src/main/java/net/rainbowcreation/core/v1_8_R3/recipe/SWarpScroll.java
@@ -4,6 +4,7 @@ import net.rainbowcreation.core.api.ICore;
 import net.rainbowcreation.core.v1_8_R3.Core;
 import net.rainbowcreation.core.v1_8_R3.utils.Item;
 import org.bukkit.Material;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 
@@ -16,12 +17,17 @@ public class SWarpScroll {
     public SWarpScroll() {
         core = Core.instance;
         namespace = "warp_scroll";
-        result = new Item().material(Material.EMPTY_MAP).displayName("<yellow>Warp Scroll").lore("<white>Teleport to selected friend").lore("<white>target player <red>MUST <white>be your friend and be in the same server").lore("<white>You can use /friend add <playername> to add player").lore("<gray>rainbowcreation.net").customModelData(1).amount(3).get();
+        result = new Item().material(Material.EMPTY_MAP).displayName("<yellow>Warp Scroll").lore("<white>Teleport to selected friend").lore("<white>target player <red>MUST <white>online in the same server").lore("<white>You can use /friend add <playername> to add player").lore("<gray>rainbowcreation.net").customModelData(1).amount(3).get();
         recipe = new ShapedRecipe(result);
         recipe.shape("010", "222", "313");
         recipe.setIngredient('0', Material.ENDER_PEARL);
         recipe.setIngredient('1', Material.INK_SACK, 5);
         recipe.setIngredient('2', Material.PAPER);
         recipe.setIngredient('3', Material.GOLD_NUGGET);
+    }
+
+    public void onUse(PlayerInteractEvent event) {
+        // show gui of friendlist
+        // when selected teleport to that player
     }
 }
