8a5fc87ca975188f58ee520ee9eaec5afddfec4c
Nathaniel Song <35866098+VocanicZ@users.noreply.github.com>
2023-12-29 18:56:49 +0700

    Delete API directory

--- API/src/main/java/net/rainbowcreation/core/api/IEvent.java
+++
@@ -1,8 +0,0 @@
-package net.rainbowcreation.core.api;
-
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
-
-public interface IEvent {
-    void register(PluginManager manager, Plugin plugin);
-}
--- API/src/main/java/net/rainbowcreation/core/api/IRegistry.java
+++
@@ -1,7 +0,0 @@
-package net.rainbowcreation.core.api;
-
-public interface IRegistry {
-    void register();
-
-    Object get(String key);
-}
